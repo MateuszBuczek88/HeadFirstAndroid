f4bd9b28937c9c6f9633682b41c81ad76ba0faa4
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2022-08-26 15:36:08 +0200

    onClickListener and logic

--- BeerAdviser/app/src/main/java/com/hfad/beeradviser/MainActivity.kt
+++ BeerAdviser/app/src/main/java/com/hfad/beeradviser/MainActivity.kt
@@ -2,10 +2,26 @@ package com.hfad.beeradviser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val findBeer = findViewById<Button>(R.id.find_beer)
+        findBeer.setOnClickListener {
+            findViewById<TextView>(R.id.brands).text = getBeers().joinToString()
+        }
+    }
+
+    private fun getBeers(): List<String> {
+        return when (findViewById<Spinner>(R.id.beer_color).selectedItem.toString()) {
+            "Light" -> listOf("Jail Pale Ale", "Lager Lite")
+            "Amber" -> listOf("Jack Amber", "Red Moose")
+            "Brown" -> listOf("Brown Bear Beer", "Bock Brownie")
+            else -> listOf("Gout Stout", "Dark Daniel")
+        }
     }
 }
