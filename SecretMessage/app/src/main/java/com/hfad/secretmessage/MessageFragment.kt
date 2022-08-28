1f37f3bf7e0f158fd249fd2c5d946c8ad30cd474
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2022-08-28 18:40:21 +0200

    navController and code refactor

--- SecretMessage/app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
+++ SecretMessage/app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
@@ -7,18 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class fragment_message : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class MessageFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_message, container, false)
     }
-
-
 }
