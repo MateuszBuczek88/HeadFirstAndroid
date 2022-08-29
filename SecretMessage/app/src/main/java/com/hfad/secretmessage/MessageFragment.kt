018d3dc231cee0f045bc01c2d145c5efc2703337
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2022-08-29 13:47:28 +0200

    passing argument between fragments

--- SecretMessage/app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
+++ SecretMessage/app/src/main/java/com/hfad/secretmessage/MessageFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.navigation.findNavController
 
 
@@ -17,12 +18,17 @@ class MessageFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_message, container, false)
         val backButton = view.findViewById<Button>(R.id.back_buton)
+        val messageView = view.findViewById<EditText>(R.id.message)
+
         backButton.setOnClickListener {
             view.findNavController().navigateUp()
         }
+
         val nextButton = view.findViewById<Button>(R.id.next_button)
         nextButton.setOnClickListener {
-            view.findNavController().navigate(R.id.encryptFragment)
+            val message = messageView.text.toString()
+            val action = MessageFragmentDirections.actionFragmentMessageToEncryptFragment(message)
+            view.findNavController().navigate(action)
         }
         return view
     }
