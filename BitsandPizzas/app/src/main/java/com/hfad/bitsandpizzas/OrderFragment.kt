d637405f460be69f6eaaf8cdb6f5141fc1da2223
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2022-08-30 17:56:21 +0200

    fab onClick

--- BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/OrderFragment.kt
+++ BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/OrderFragment.kt
@@ -5,9 +5,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class OrderFragment : Fragment() {
 
@@ -20,6 +26,20 @@ class OrderFragment : Fragment() {
         val toolbar = view.findViewById<MaterialToolbar>(R.id.tool_bar)
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
 
+
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
+
+        fab.setOnClickListener {
+            val pizzaGroup = view.findViewById<RadioGroup>(R.id.pizza_group)
+            val pizzaType = pizzaGroup.checkedRadioButtonId
+            if (pizzaType == -1) {
+                Toast.makeText(activity, "No pizza type selected", Toast.LENGTH_SHORT).show()
+
+            } else {
+                var text = view.findViewById<Button>(pizzaGroup.checkedRadioButtonId).text
+                Snackbar.make(fab, text, Snackbar.LENGTH_SHORT).show()
+            }
+        }
         return view
     }
 }
