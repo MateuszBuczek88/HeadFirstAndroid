b3907d092d6f139fca0cc77bf48689578a4394ad
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2022-09-02 14:12:22 +0200

    private values and getters

--- GuessingGame/app/src/main/java/com/hfad/guessinggame/GameViewModel.kt
+++ GuessingGame/app/src/main/java/com/hfad/guessinggame/GameViewModel.kt
@@ -1,21 +1,30 @@
 package com.hfad.guessinggame
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GameViewModel: ViewModel() {
-    val words = listOf("ANDROID", "ACTIVITY", "FRAGMENT")
-    val secredWord = words.random().uppercase()
-    val secredWordDisplay = MutableLiveData<String>()
-    var correctGuesses = ""
-    val incorrectGuesses = MutableLiveData<String>("")
-    val livesLeft = MutableLiveData<Int>(8)
+class GameViewModel : ViewModel() {
+    private val words = listOf("ANDROID", "ACTIVITY", "FRAGMENT")
+    private val secredWord = words.random().uppercase()
+    private var correctGuesses = ""
+    private val _secredWordDisplay = MutableLiveData<String>()
+    val secredWordDisplay: LiveData<String>
+        get() = _secredWordDisplay
+
+    private val _incorrectGuesses = MutableLiveData<String>("")
+    val incorrectGuesses: LiveData<String>
+        get() = _incorrectGuesses
+
+    private val _livesLeft = MutableLiveData<Int>(8)
+    val livesLeft: LiveData<Int>
+        get() = _livesLeft
 
     init {
-        secredWordDisplay.value = deriveWordDisplay()
+        _secredWordDisplay.value = deriveWordDisplay()
     }
 
-    fun deriveWordDisplay(): String {
+    private fun deriveWordDisplay(): String {
         var display = ""
         secredWord.forEach {
             display += checkLetter(it.toString())
@@ -23,27 +32,29 @@ class GameViewModel: ViewModel() {
         return display
     }
 
-    fun checkLetter(str: String) =
+    private fun checkLetter(str: String) =
         when (correctGuesses.contains(str)) {
             true -> str
             false -> "_"
         }
+
     fun makeGuess(guess: String) {
         if (guess.length == 1) {
             if (secredWord.contains(guess)) {
                 correctGuesses += guess
-                secredWordDisplay.value = deriveWordDisplay()
+                _secredWordDisplay.value = deriveWordDisplay()
             } else {
-                incorrectGuesses.value += "$guess"
+                _incorrectGuesses.value += "$guess"
                 livesLeft.value?.minus(1)
             }
         }
     }
-    fun isWon() = secredWord.equals(secredWordDisplay.value,true)
+
+    fun isWon() = secredWord.equals(secredWordDisplay.value, true)
     fun isLost() = (livesLeft.value ?: 0) <= 0
-    fun wonLostMessage() :String{
-        var message =""
-        if(isWon()) message = "You Won"
+    fun wonLostMessage(): String {
+        var message = ""
+        if (isWon()) message = "You Won"
         else if (isLost()) message = "You Lost"
         message += "The word was $secredWord"
         return message
