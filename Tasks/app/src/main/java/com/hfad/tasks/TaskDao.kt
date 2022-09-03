1c07a0ed31b7e8ce64a0ace6f440be8cb4967c85
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2022-09-03 15:35:11 +0200

    create datanase instance

--- Tasks/app/src/main/java/com/hfad/tasks/TaskDao.kt
+++ Tasks/app/src/main/java/com/hfad/tasks/TaskDao.kt
@@ -6,13 +6,13 @@ import androidx.room.*
 @Dao
 interface TaskDao {
     @Insert
-    fun insert(task: Task)
+    suspend fun insert(task: Task)
 
     @Update
-    fun update(task: Task)
+    suspend fun update(task: Task)
 
     @Delete
-    fun delete(task: Task)
+    suspend fun delete(task: Task)
 
     @Query("SELECT * FROM task_table WHERE taskId = :taskID")
     fun get(taskID: Long): LiveData<Task>
