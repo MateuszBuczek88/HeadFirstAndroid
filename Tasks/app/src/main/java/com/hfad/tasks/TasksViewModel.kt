bd6bb07d4ac49565fbc0da46e45054fa9b50ddb7
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2022-09-03 15:45:30 +0200

    view model factory

--- Tasks/app/src/main/java/com/hfad/tasks/TasksViewModel.kt
+++ Tasks/app/src/main/java/com/hfad/tasks/TasksViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
-class TaskViewModel(val dao: TaskDao) : ViewModel() {
+class TasksViewModel(val dao: TaskDao) : ViewModel() {
     var newTaskName = ""
     fun addTask() {
         viewModelScope.launch {
