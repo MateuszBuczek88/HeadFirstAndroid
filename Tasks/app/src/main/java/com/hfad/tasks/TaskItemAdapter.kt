470068c754ab33b2e6dbe3b3e7a3333a8ac8dd4e
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2022-09-04 15:27:47 +0200

    change xml for recycler view

--- Tasks/app/src/main/java/com/hfad/tasks/TaskItemAdapter.kt
+++ Tasks/app/src/main/java/com/hfad/tasks/TaskItemAdapter.kt
@@ -4,6 +4,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
+import android.widget.CheckBox
+import androidx.cardview.widget.CardView
 
 
 class TaskItemAdapter : RecyclerView.Adapter<TaskItemAdapter.TaskItemViewHolder>() {
@@ -13,29 +15,30 @@ class TaskItemAdapter : RecyclerView.Adapter<TaskItemAdapter.TaskItemViewHolder>
             notifyDataSetChanged()
         }
 
-    override fun onBindViewHolder(holder: TaskItemViewHolder, position: Int) {
-        val item = data[position]
-        holder.bind(item)
-
-    }
-
-    class TaskItemViewHolder(val rootView: TextView) : RecyclerView.ViewHolder(rootView) {
-        fun bind(item: Task) {
-            rootView.text = item.taskName
-        }
-
-        companion object {
-            fun inflateFrom(parent: ViewGroup): TaskItemViewHolder {
-                val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.task_item, parent, false) as TextView
-                return TaskItemViewHolder(view)
-            }
-        }
-    }
-
     override fun getItemCount() = data.size
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskItemViewHolder =
         TaskItemViewHolder.inflateFrom(parent)
 
+    override fun onBindViewHolder(holder: TaskItemViewHolder, position: Int) {
+        val item = data[position]
+        holder.bind(item)
+    }
 
+    class TaskItemViewHolder(val rootView: CardView) : RecyclerView.ViewHolder(rootView) {
+
+        val taskName = rootView.findViewById<TextView>(R.id.task_name)
+        val taskDone = rootView.findViewById<CheckBox>(R.id.task_done)
+
+        companion object {
+            fun inflateFrom(parent: ViewGroup): TaskItemViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater.inflate(R.layout.task_item, parent, false) as CardView
+                return TaskItemViewHolder(view)
+            }
+        }
+        fun bind(item: Task) {
+            taskName.text = item.taskName
+            taskDone.isChecked = item.taskDone
+        }
+    }
 }
